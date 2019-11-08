5a99f24f334a62ded4ef524eb00d157bc1ad70a4
pepesan <pepesan@gmail.com>
2019-11-08 10:46:22 +0100

    meter ficheros estáticos en la aplicación

--- miappdjango/urls.py
+++ miappdjango/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from miapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 #import miapp
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('miapp.urls'))
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
